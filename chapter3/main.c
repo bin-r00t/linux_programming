95b87402f0887929756ca98919461c9d49e50828
bin-r00t <bh0bama@sina.com>
2023-09-19 21:26:24 +0800

    add ignore

---
+++ chapter3/main.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+int main()
+{
+
+  char buffer[20]; // read 20 bytes
+  int nread;
+
+  nread = read(0, buffer, 20);
+  if (nread == -1)
+  {
+    /** something went wrong, check <errno> */
+    write(2, "A read error has occurred\n", 26);
+  }
+
+  if ((write(1, buffer, nread)) != nread)
+  {
+    write(2, "A write error has occurred\n", 27);
+  }
+
+  /*
+    $ echo hello languageC | ./a.out
+    or
+    $ ./a.out
+    then enter something
+    try 24 characters:
+    liubin@ubuntu18:~/Documents/coding/linux_programming/chapter3$ ./a.out
+      111122223333444455556666
+      11112222333344445555liubin@ubuntu18:~/Documents/coding/linux_programming/chapter3$ 6666
+      6666: command not found
+  */
+
+  exit(0);
+}
